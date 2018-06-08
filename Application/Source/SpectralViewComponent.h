595ca96c0c1bd981b47fbdf1c511f1d7e09be8a1
Unknown <nicholaslenz12@g.ucla.edu>
2018-06-07 17:43:31 -0700

    Added more info to the research file. Auto-generated basis for new class

---
+++ Application/Source/SpectralViewComponent.h
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+    SpectralViewComponent.h
+    Author:  Nicholas Lenz
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+*/
+class SpectralViewComponent    : public Component
+{
+public:
+    SpectralViewComponent();
+    ~SpectralViewComponent();
+
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SpectralViewComponent)
+};
